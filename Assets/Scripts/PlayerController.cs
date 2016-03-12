5ad44cebe3353abb2b138a45e9ac5e5baa2eebf2
AbosPubsNG <abos.pubs.ng@gmail.com>
2016-03-12 23:24:35 +0100

    Ajout du déplacement avant et arrière sur le player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Force = 10;
+    private Rigidbody _rigidBody = null;
+    private Vector3 _movement;
+
+	void Start ()
+    {
+        this._rigidBody = this.GetComponent<Rigidbody>();
+	}
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log(@"update : UpArrow");
+            _movement = transform.forward * Force;   
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log(@"update : DownArrow");
+            _movement = transform.forward * -Force;
+        }
+    }
+	
+	void FixedUpdate ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log(@"FixedUpdate : Arrow");
+            this._rigidBody.MovePosition(_rigidBody.position + _movement);
+        }
+	}
+}
