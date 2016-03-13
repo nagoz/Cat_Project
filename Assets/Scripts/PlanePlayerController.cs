327aa964b32cb1a9084981277bde9334fd503054
AbosPubsNG <abos.pubs.ng@gmail.com>
2016-03-13 23:27:01 +0100

    Suppression maincamera et directionnallight. Ajout de la rotation du player Essai de masque d'affichage de la vision du player

---
+++ Assets/Scripts/PlanePlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanePlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(@"enter collider");
+        other.gameObject.layer = 10;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(@"exit collider");
+        other.gameObject.layer = 0;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,36 +3,56 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Force = 10;
+    public float Force = 0.2f;
+    public float Rotation = 1f;
     private Rigidbody _rigidBody = null;
     private Vector3 _movement;
+    private Quaternion _rotation;
 
 	void Start ()
     {
-        this._rigidBody = this.GetComponent<Rigidbody>();
+        _rigidBody = this.GetComponent<Rigidbody>();
 	}
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.Z))
         {
-            Debug.Log(@"update : UpArrow");
+            Debug.Log(@"update : Z");
             _movement = transform.forward * Force;   
         }
 
-        if(Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetKey(KeyCode.S))
         {
-            Debug.Log(@"update : DownArrow");
+            Debug.Log(@"update : S");
             _movement = transform.forward * -Force;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.Log(@"update : Q");
+            _rotation = Quaternion.Euler(0f, -Rotation, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log(@"update : D");
+            _rotation = Quaternion.Euler(0f, Rotation, 0f);
         }
     }
 	
 	void FixedUpdate ()
     {
-        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.S))
         {
-            Debug.Log(@"FixedUpdate : Arrow");
+            Debug.Log(@"FixedUpdate : Z ou S");
             this._rigidBody.MovePosition(_rigidBody.position + _movement);
+        }
+
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+        {
+            Debug.Log(@"FixedUpdate : Q ou D");
+            this._rigidBody.MoveRotation(_rigidBody.rotation * _rotation);
         }
 	}
 }
